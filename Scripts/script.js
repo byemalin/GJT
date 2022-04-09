db4420d8424d6d6ea6b7489966b340a59e1ee9bc
Brittany Price <bprice0496@gmail.com>
2022-04-09 16:47:08 -0400

    I made the waitForHost() screen but the game will not start after mouse press nor pressing enter.

--- Scripts/script.js
+++ Scripts/script.js
@@ -13,22 +13,50 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(600, 600);
+  var mainCanvas = createCanvas(800, 800);
+  mainCanvas.parent("canvasdiv");
+  //Toggle Server Info
+  partyToggleInfo(false);
+  toggle = document.getElementById('toggle');
 
   //player 1 score
   shared.score1 = shared.score1 || 0;
   //player 2 score
   shared.score2 = shared.score2 || 0;
+
+  if(partyIsHost()){
+    partySetShared(shared,{
+        hostStart: false,
+        hostRestart: false
+    });
+  }
 }
 
 function draw() {
-  // Display the contents of the current screen
+  background(96, 157, 255);
+  // switch(screen){
+  //   case 1:
+  //     waitForHost();
+  //     break;
+  //   case 2:
+  //     gameOn();
+  //     break;
+  //   case 3:
+  //     gameOver(); 
+  //   default:
+  //     startScreen();
+  //     break;
+  // }
+  // // Display the contents of the current screen
   if (screen == 0) {
     startScreen();
   } else if (screen == 1) {
-    gameOn();
+    waitForHost();
     // if the screen variable was changed to 2, show the game over screen
   } else if (screen == 2) {
+    gameOn();
+  }
+  else if(screen == 3){
     gameOver();
   }
 }
@@ -36,7 +64,12 @@ function draw() {
 function mousePressed() {
   if (screen == 0) {
     screen = 1;
-  } else if (screen == 1) {
+  }
+  else if(screen == 1){
+    screen == 2;
+  }
+  
+  else if (screen == 2) {
     if (
       mouseX > x &&
       mouseX < x + 50 &&
@@ -47,7 +80,7 @@ function mousePressed() {
 
       if (colorPicker == 0) {
         // red click = game over
-        screen = 2;
+        screen = 3;
       } else {
         //speed is controlled locally
         speed += 0.05;
@@ -61,22 +94,48 @@ function mousePressed() {
         }
       }
     }
-  } else if (screen == 2) {
+  } else if (screen == 3) {
     screen = 0;
   }
 }
 
 function startScreen() {
-  background(96, 157, 255);
+  textSize(30);
+
   fill(255);
   textAlign(CENTER);
-  text("WELCOME SCREEN HERE", width / 2, height / 2);
-  text(
-    "Feel free to change the scene code method ofcourse!",
-    width / 2,
-    height / 2 + 20
-  );
+  text("GJT Wac-A-Mole",width/2,height/2 + 20);
+  text("Click to begin", width/2, height/2 + 60);
+  // text("WELCOME SCREEN HERE", width / 2, height / 2);
+  // text(
+  //   "Feel free to change the scene code method of course!",
+  //   width / 2,
+  //   height / 2 + 20
+  // );
   reset();
+}
+
+function waitForHost(){
+  textSize(40);
+  fill(255);
+  if(partyIsHost()){
+    text("You are the Host", 250, 250);
+    text("Press ENTER to begin", 250, 300);
+    text("when everyone is ready", 250, 350);
+    //enter
+    if(keyCode == 13){
+      shared.hostStart = true;
+      shared.score1 = 0;
+      shared.score2 = 0;
+      scene = 2;
+    }
+  }
+  else if(!partyIsHost()){
+    text("Waiting for the host", 250, 250);
+    if(shared.hostStart == true){
+      scene = 2;
+    }
+  }
 }
 
 function gameOn() {
